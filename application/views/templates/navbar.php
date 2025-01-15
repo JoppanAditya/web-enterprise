82836a0f3457bc97fe03c88abb1a1292cacfe198
JoppanAditya <github.unfixed012@passmail.com>
2025-01-15 08:11:19 +0700

    change image

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -68,9 +68,9 @@
                      </div>
                  </li> <!--end::Notifications Dropdown Menu--> <!--begin::Fullscreen Toggle-->
                  <li class="nav-item"> <a class="nav-link" href="javascript:void(0);" data-lte-toggle="fullscreen"> <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i> <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none;"></i> </a> </li> <!--end::Fullscreen Toggle--> <!--begin::User Menu Dropdown-->
-                 <li class="nav-item dropdown user-menu"> <a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <img src="<?= base_url('assets/'); ?>img/user2-160x160.jpg" class="user-image rounded-circle shadow" alt="User Image"> <span class="d-none d-md-inline"><?= $this->session->userdata('username'); ?></span> </a>
+                 <li class="nav-item dropdown user-menu"> <a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <img src="<?= base_url('assets/img/') . $this->session->userdata('foto'); ?>" class="user-image rounded-circle shadow" alt="User Image"> <span class="d-none d-md-inline"><?= $this->session->userdata('username'); ?></span> </a>
                      <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
-                         <li class="user-header text-bg-primary"> <img src="<?= base_url('assets/'); ?>img/user2-160x160.jpg" class="rounded-circle shadow" alt="User Image">
+                         <li class="user-header text-bg-primary"> <img src="<?= base_url('assets/img/') . $this->session->userdata('foto'); ?>" class="rounded-circle shadow" alt="User Image">
                              <p>
                                  <?= $this->session->userdata('username'); ?>
                                  <small><?= $this->session->userdata('role'); ?></small>
